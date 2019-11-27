648d53a6e80cab022e0ed184da83d456efb486b4
Marc BM <marcbajona1@gmail.com>
2019-11-27 13:34:37 +0100

    Commit 4

--- app/src/main/java/com/bajonacastella/numerosecret/MainActivity.java
+++ app/src/main/java/com/bajonacastella/numerosecret/MainActivity.java
@@ -3,12 +3,28 @@ package com.bajonacastella.numerosecret;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int numeroSecret;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Generem un número aleatori entre 0 i 1000 amb el Random que ens dona Java.
+        Random ns = new Random();
+        numeroSecret = ns.nextInt(1000);
+
+
+    }
+
+    public void botoEndevinar(View v) {
+        Toast.makeText(this,"El número és: " + numeroSecret, Toast.LENGTH_LONG).show();
     }
 }
