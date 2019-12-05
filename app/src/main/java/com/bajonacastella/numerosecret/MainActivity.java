4695c71e920d605c12710712d8600e5402cd7dbe
Marc BM <marcbajona1@gmail.com>
2019-12-05 17:59:28 +0100

    Feina 5 Desembre - v1

--- app/src/main/java/com/bajonacastella/numerosecret/MainActivity.java
+++ app/src/main/java/com/bajonacastella/numerosecret/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnEstablirTemps;
     private TextView txtFeedbackUsuari, txtTempsRestant;
     private static String[] feedback = {"El número secret és més petit","El número secret és més gran", "Has ENCERTAT el número secret!"};
+    private Random ns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +42,11 @@ public class MainActivity extends AppCompatActivity {
         txtTempsRestant = findViewById(R.id.txtTempsR);
 
         //Generem un número aleatori entre 0 i 1000 amb el Random que ens dona Java.
-        Random ns = new Random();
+        ns = new Random();
         numeroSecret = ns.nextInt(1000);
+
+        btnJugar.setEnabled(false);
+        edNumUsuari.setEnabled(false);
 
 
     }
@@ -52,16 +56,20 @@ public class MainActivity extends AppCompatActivity {
         if(numUsuari < numeroSecret) {
             //Toast.makeText(this,"El número que has introduït és més petit", Toast.LENGTH_LONG).show();
             txtFeedbackUsuari.setText(feedback[1]);
+            txtFeedbackUsuari.setTextColor(Color.RED);
         }
         else if (numUsuari > numeroSecret) {
             //Toast.makeText(this,"El número que has introduït és més gran", Toast.LENGTH_LONG).show();
             txtFeedbackUsuari.setText(feedback[0]);
+            txtFeedbackUsuari.setTextColor(Color.RED);
         }
         else {
             //Toast.makeText(this,"El número que has introduït és el NÚMERO SECRET!", Toast.LENGTH_LONG).show();
             txtFeedbackUsuari.setText(feedback[2]);
-            numeroEndevinat = true;
+            txtFeedbackUsuari.setTextColor(Color.RED);
+            txtTempsRestant.setTextColor(Color.rgb(0,133,119));
 
+            numeroEndevinat = true;
             btnJugar.setEnabled(false);
             edNumUsuari.setEnabled(false);
         }
@@ -80,6 +88,9 @@ public class MainActivity extends AppCompatActivity {
         protected void onPreExecute() {
             btnEstablirTemps.setEnabled(false);
             npTemps.setEnabled(false);
+            btnJugar.setEnabled(true);
+            edNumUsuari.setEnabled(true);
+
         }
 
         @Override
@@ -98,20 +109,28 @@ public class MainActivity extends AppCompatActivity {
         protected void onProgressUpdate(Integer... values) {
             int segonsRestants = values[0];
             int segonsInicials = npTemps.getValue();
-            if((segonsRestants*100/segonsInicials) >= 50) {
-                txtTempsRestant.setTextColor(Color.GREEN);
+            if((segonsRestants*100/segonsInicials) > 50) {
+                txtTempsRestant.setTextColor(Color.rgb(0,133,119));
+                txtTempsRestant.setText("Temps restant: "+ segonsRestants + "s");
             } else if ((segonsRestants*100/segonsInicials) <= 50  &&(segonsRestants*100/segonsInicials) >= 25) {
-                txtTempsRestant.setTextColor(Color.YELLOW);
+                txtTempsRestant.setText("Temps restant: "+ (segonsRestants*10) + "ms");
+                txtTempsRestant.setTextColor(Color.rgb(192,123,65));
             } else {
+                txtTempsRestant.setText("Temps restant: "+ (segonsRestants*100) + "cs");
                 txtTempsRestant.setTextColor(Color.RED);
             }
-            txtTempsRestant.setText("Temps restant: "+ segonsRestants);
+
         }
 
         @Override
         protected void onPostExecute(Long aLong) {
-            //txResultat.setText(String.valueOf(aLong));
-           // btnCFac.setEnabled(true);
+            txtFeedbackUsuari.setText("S'ha acabat el temps! Torna a jugar.");
+            txtFeedbackUsuari.setTextColor(Color.RED);
+            numeroSecret = ns.nextInt(1000);
+            btnEstablirTemps.setEnabled(true);
+            npTemps.setEnabled(true);
+            btnJugar.setEnabled(false);
+            edNumUsuari.setEnabled(false);
         }
     }
 
